0548706d57dfbc37544849b9002a9d4ca99e11f5
Rosamaria Rodriguez <rous9716@gmail.com>
2022-01-12 14:02:43 -0500

    Build: Add case for unique, total and broken links

---
+++ src/options.js
@@ -0,0 +1,33 @@
+const chalkStyle = require('chalk');
+
+// functions for get count status (total, unique, broken)
+  // new Set se usa para obtener elementos únicos en un array
+  // ... para evitar que se cree otro []
+  
+// status: total
+const totalLinks = (array) => {
+  const total = array.length;
+  const msgTotal = chalkStyle.italic.blue(`Total: ${total}`);
+  return `${msgTotal}`;
+}
+
+// status: unique
+const uniqueLinks = (array) => {
+  let unique = array.map((link) => link.href);
+  unique = [...new Set(unique)].length;
+  const msgUnique = chalkStyle.italic.cyan(`Unique: ${unique}`);
+  return `\n${msgUnique}`;
+}
+
+// status: broken
+const brokenLinks = (array) => {
+  const broken = array.filter((link) => link.statusText === 'Fail');
+  const msgBroken = chalkStyle.italic.red(`Broken: ${broken.length}`);
+  return `\n${msgBroken}`;
+};
+
+module.exports = {
+  totalLinks,
+  uniqueLinks,
+  brokenLinks
+};
