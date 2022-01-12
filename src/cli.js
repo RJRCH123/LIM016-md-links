4df8e01f2c69f3e44ffc542c1873e57144246dd6
Rosamaria Rodriguez <rous9716@gmail.com>
2022-01-12 14:17:53 -0500

    Text: Add name fuction mdLinksFunction

---
+++ src/cli.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+const chalkStyle = require('chalk');
+const customStyle = chalkStyle.italic;
+const {   
+  totalLinks,
+  uniqueLinks,
+  brokenLinks 
+} = require('./options.js');
+
+const mdLinksFunction = require('./md-links.js');
+
+// For use arguments
+const [, , ...argument] = process.argv;
+
+
+// functions for content of comands
+
+// Si se encuentra un path //
+
+if (argument.length === 1) {
+  mdLinksFunction(argument[0], { validate: false })
+  .then(resolve => {
+    resolve.map((objeto) => {
+    console.log(customStyle.blue(`${objeto.file} | ${objeto.text} | ${objeto.href}`));
+    })
+  })
+  .catch(reject => console.log(reject));
+}
+
+// Si se encuentra un path y una option //
+
+if (argument.length === 2) {
+  switch (argument[1]) {
+  case '--validate':
+    mdLinks(argument[0], { validate: true })
+    .then(resolve => {
+      resolve.map((objeto) => {
+      console.log(customStyle.blue(`
+      ${objeto.file} | ${objeto.text} |
+      ${objeto.href} | ${objeto.statusText} | ${objeto.message}`));
+      })
+    })
+    .catch(reject => console.log(customStyle.red(reject)));
+    break;
+
+  case '--stats':
+    mdLinks(argument[0], { validate: true })
+      .then(resolve => console.log(`${totalLinks(resolve)} \nUnique: ${uniqueLinks(resolve)}`))
+      .catch(reject => console.log(reject));
+    break;
+
+  case '--help':
+    console.log(customStyle.yellow(`
+    +____________________+_________________________________________________________+
+    |      Comands       |                         Output                          |
+    +____________________+_________________________________________________________+
+    | md path            | Print href, text and file.                              |
+    +____________________+_________________________________________________________+
+    | --stats            | Print total and unique links .                          |
+    +____________________+_________________________________________________________+
+    | --validate         | Print href, text, file, message(ok or fail) and status. |
+    +____________________+_________________________________________________________+
+    | --validate --stats | Print total, unique and broken links.                   |
+    +____________________+_________________________________________________________+
+    | --stats --validate | Print total, unique and broken links.                   |
+    +____________________+_________________________________________________________+
+    | --help             | Print comands list.                                     |
+    +____________________+_________________________________________________________+
+    `))
+    break;
+
+  default: console.log(customStyle.red('Invalid comand. If you need help, please type --help'));
+    break;
+  }
+}
+
+// Si se encuentra un path y dos options //
+
+if (argument.length === 3) {
+  if (
+    (argument[1] === "--stats" && argument[2] === "--validate") ||
+    (argument[1] === "--validate" && argument[2] === "--stats")
+  ) {
+    mdLinks(argument[0], { validate: true })
+      .then(resolve => console.log(`${totalLinks(resolve)} \nUnique: ${uniqueLinks(resolve)} ${brokenLinks(resolve)}`))
+      .catch(reject => console.log(customStyle.red(reject)));
+  } else {
+    console.log(customStyle.red('Invalid comand. If you need help, please type --help'));
+  }
+}
+
+// console.log(process.argv);
